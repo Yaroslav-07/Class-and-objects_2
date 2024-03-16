23c3a17e60462a5f01d3d149413d7d88ce68b466
Yaroslav <yar0slav03@mail.com>
2024-03-16 12:08:00 +0400

    init

---
+++ Class and objects_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+	class Counter
+	{
+	public:
+		Counter(int count = 1) {
+			this->count = count;
+		};
+		void next() {
+			this->count = this->count++;
+		}
+		void previous (){
+			this->count = this->count--;
+		}
+		int shouw() {
+			return this->count;
+		}
+
+	private:
+		int count;
+	};
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+    std::cout << "Начало работы счетчика: \n";
+	Counter c1;
+	std::cout<<c1.shouw()<< std::endl;
+	c1.next();
+	std::cout << c1.shouw() << std::endl;
+	c1.previous();
+	std::cout << c1.shouw() << std::endl;
+	Counter c2{ 5 };
+	std::cout << c2.shouw() << std::endl;
+}
