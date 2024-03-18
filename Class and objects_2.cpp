2ed321c622d9648fbd3781ced23eb3801a25224d
Yaroslav <yar0slav03@mail.com>
2024-03-18 09:58:36 +0400

    fix_1

--- Class and objects_2.cpp
+++ Class and objects_2.cpp
@@ -19,16 +19,56 @@
 	private:
 		int count;
 	};
+	Counter for_start() {
+		std::string choice;
+		std::cout << "Вы хотите указать начальное значение счётчика? Введите yes или no: ";
+		std::cin >> choice;
+		if (choice == "yes") {
+			int n;
+			std::cout << "Введите начальное значение счётчика: ";
+			std::cin >> n;
+			Counter c1{ n };
+			return c1;
+	    }
+	    else {
+			Counter c1;
+			return c1;
+	    }
+	}
+
+
 
 int main(){
 	setlocale(LC_ALL, "Russian");
-    std::cout << "Начало работы счетчика: \n";
-	Counter c1;
-	std::cout<<c1.shouw()<< std::endl;
-	c1.next();
-	std::cout << c1.shouw() << std::endl;
-	c1.previous();
-	std::cout << c1.shouw() << std::endl;
-	Counter c2{ 5 };
-	std::cout << c2.shouw() << std::endl;
+	Counter c1 = for_start();
+
+	while (true)
+	{
+		char choice = '-';
+
+		
+		while (choice == '+' || choice == '-' || choice == '='){
+			std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+			std::cin >> choice;
+			switch (choice){
+			case '+': {
+				c1.next();
+				break;
+			}
+			case '-': {
+				c1.previous();
+				break;
+			}
+			case '=': {
+				std::cout << c1.shouw() << std::endl;
+				break;
+			}
+			}
+		}
+			std::cout << "До свидания!";
+
+	}
+	
+
+	
 }
